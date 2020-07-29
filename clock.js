96e8389d36a0e0e6a5c1007e5a63cbd54c74c21c
Pranil <bhosalepranil16@gmail.com>
2020-07-29 13:05:43 +0530

    init

---
+++ clock.js
@@ -0,0 +1,44 @@
+{
+    var displayTime = function() {
+        var date,hrs,min,sec;
+        date = new Date();
+        hrs = formated(date.getHours());
+        min = formated(date.getMinutes());
+        sec = formated(date.getSeconds());
+        document.querySelector('.clock-body').textContent = hrs + ' : ' + min + ' : ' + sec;
+        setTimeout(displayTime,1000);
+        
+    }
+    var displayDate = function(){
+        var date,months,year,month,day,d,type;
+        months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+        date = new Date();
+        day = formated(date.getDate());
+        month = months[date.getMonth()];
+        year = date.getFullYear();
+        if(date.getDate() === 1)
+            type = 'st';
+        else if(date.getDate() === 2)
+            type = 'nd';
+        else if(date.getDate() === 3)
+            type = 'rd';
+        else
+            type = 'th';
+        document.querySelector('.date-body').textContent = day + type +' ' + month + ' ' + year; 
+        d = date.getDay();
+        document.querySelector('#day-' + d).classList.add('hi');
+    }
+    var formated = function(el) {
+        if(el < 10)
+            return '0' + el;
+        else
+            return el;
+    }
+    displayTime();
+    displayDate();
+    var counter = function() {
+        var arr = [0,0,0];
+        
+    }
+}
+
